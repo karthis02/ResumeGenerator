f68b8e9f9e77adce5ff2802113337e8b9d747e3b
karthis02 <karthi.s02@infosys.com>
2019-09-11 15:10:26 +0530

    Initial Commit

---
+++ ResumeGenerator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ResumeGenerator
+//
+//  Created by Karthi Samigoundanpalayamshanmugam02 on 11/09/19.
+//  Copyright © 2019 Karthi Samigoundanpalayamshanmugam02. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
